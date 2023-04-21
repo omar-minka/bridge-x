2e31db668c6749d7d0baa3cc42da84ffdccf3314
Damir Juretic <>
2023-04-21 11:17:59 +0200

    debit refactored

--- src/adapters/async-debit.adapter.ts
+++ src/adapters/async-debit.adapter.ts
@@ -2,36 +2,18 @@ import {
   AbortResult,
   AbortSucceededResult,
   CommitResult,
-  CommitSucceededResult,
   IBankAdapter,
-  JobResultStatus, JobSuspendedResult,
+  JobResultStatus,
   PrepareResult,
   TransactionContext,
-} from '@minka/bridge-sdk'
-import {LedgerSdk} from "@minka/ledger-sdk";
-import {CoopcentralApiService} from "../coopcentral-api-service";
-import {AccountDetailsRequest, CheckBankAccountRequest, CreateBankTransactionRequest, ServiceError } from "../models";
-import { config } from '../config'
-import {PrepareFailedResult} from "@minka/bridge-sdk/src/bank/bank-adapter.service";
-import {LedgerWallet} from "@minka/bridge-sdk/types";
+} from "@minka/bridge-sdk";
+import { PrepareFailedResult } from "@minka/bridge-sdk/src/bank/bank-adapter.service";
+import { LedgerSdk } from "@minka/ledger-sdk";
+import { CoopcentralApiService } from "../coopcentral-api-service";
+import { CreateBankTransactionRequest, ServiceError } from "../models";
+import { config } from "../config";
 
-const suspendedJobs = new Map()
-
-function createBankTransactionRequest(context: TransactionContext, wallet: any) {
-  const transactionRequest = new CreateBankTransactionRequest()
-  transactionRequest.idTxEntidad = context.intent.handle
-  transactionRequest.valorTx = `${context.entry.amount}`
-  transactionRequest.descripTx = 'Testing'
-
-  const virtualAccount = `${config.COOPCENTRAL_VIRTUAL_ACCOUNT}`
-
-  transactionRequest.nomOrig = wallet.custom.name || 'Test'
-  transactionRequest.docProdOrig = wallet.custom.document
-  transactionRequest.prodOrig = wallet.custom.account
-  transactionRequest.prodDest = virtualAccount
-
-  return transactionRequest
-}
+const suspendedJobs = new Map();
 
 /**
  * Demo implementation of async debit bank adapter
@@ -45,205 +27,203 @@ function createBankTransactionRequest(context: TransactionContext, wallet: any) 
  * results.
  */
 export class AsyncDebitBankAdapter extends IBankAdapter {
-
-  constructor(private readonly ledger: LedgerSdk, private readonly coopcentralApi: CoopcentralApiService) {
+  constructor(
+    private readonly ledger: LedgerSdk,
+    private readonly coopcentralApi: CoopcentralApiService
+  ) {
     super();
-
-
   }
 
   async prepareAsync(context: TransactionContext) {
-    const jobId = context.job.handle
-    const job = suspendedJobs.get(jobId)
-
-
-    if (!job || job.status === 'RUNNING') {
-      return
-    }
-
-    job.status = 'RUNNING'
-
-    const { wallet } = await this.ledger.wallet.read(context.entry.source)
-
-    try {
-      let bankAccountResult = await this.coopcentralApi.checkBankAccount(
-          new CheckBankAccountRequest(
-              wallet.custom.account,
-              wallet.custom.document,
-              wallet.custom.documentType,
-          ),
-      )
-
-      let bankAccountDetails = await this.coopcentralApi.fetchAccountDetails(
-          new AccountDetailsRequest(
-              wallet.custom.document,
-              wallet.custom.documentType,
-          ),
-      )
-
-      console.log(JSON.stringify(bankAccountDetails), JSON.stringify(bankAccountResult))
-    } catch (error) {
-      job.status = 'FAILED'
-      job.error = error
-    }
-
-    job.status = 'COMPLETED'
-  }
-
-  async commitAsync(context: TransactionContext) {
-    const jobId = context.job.handle
-    const job = suspendedJobs.get(jobId)
+    const jobId = context.job.handle;
+    const job = suspendedJobs.get(jobId);
 
     if (!job) {
-      return
+      return;
     }
 
-    if (job.status === 'RUNNING') {
+    if (job.status === "RUNNING") {
       // call coopcentral transaction status
-      return
+      return;
     }
 
-    job.status = 'RUNNING'
-
-    const { wallet } = await this.ledger.wallet.read(context.entry.source)
+    job.status = "RUNNING";
 
     try {
-      const transactionRequest = createBankTransactionRequest(context, wallet)
-      const bankTransaction = await this.coopcentralApi.createBankTransaction(
-          transactionRequest,
-      )
+      const { wallet } = await this.ledger.wallet.read(context.entry.source);
 
-      console.log(JSON.stringify(bankTransaction))
-      job.status = 'COMPLETED'
+      const transactionRequest = new CreateBankTransactionRequest();
+      transactionRequest.idTxEntidad = context.intent.handle;
+      transactionRequest.valorTx = `${context.entry.amount}`;
+      transactionRequest.descripTx = "Prepare";
+      transactionRequest.nomOrig = wallet.custom.name || "Test";
+      transactionRequest.docProdOrig = wallet.custom.document;
+      transactionRequest.prodOrig = wallet.custom.account;
+      transactionRequest.prodDest = `${config.COOPCENTRAL_VIRTUAL_ACCOUNT}`;
+
+      const bankTransaction = await this.coopcentralApi.createBankTransaction(
+        transactionRequest
+      );
+
+      console.log(JSON.stringify(bankTransaction));
+      job.status = "COMPLETED";
     } catch (error) {
       if (error instanceof ServiceError) {
         if (error.isPending()) {
-          return
+          return;
+        }
+
+        if (error.isRetryable()) {
+          job.status = "PENDING";
+          return;
         }
       }
 
-      job.status = 'PENDING'
+      job.status = "ERROR";
+      job.error = error;
+    }
+  }
+
+  async abortAsync(context: TransactionContext) {
+    const jobId = context.job.handle;
+    const job = suspendedJobs.get(jobId);
+
+    if (!job) {
+      return;
+    }
+
+    if (job.status === "RUNNING") {
+      // call coopcentral transaction status
+      return;
+    }
+
+    job.status = "RUNNING";
+
+    try {
+      const { wallet } = await this.ledger.wallet.read(context.entry.source);
+
+      const transactionRequest = new CreateBankTransactionRequest();
+      transactionRequest.idTxEntidad = context.intent.handle;
+      transactionRequest.valorTx = `${context.entry.amount}`;
+      transactionRequest.descripTx = "Abort";
+      transactionRequest.nomDest = wallet.custom.name || "Test";
+      transactionRequest.docProdDest = wallet.custom.document;
+      transactionRequest.prodDest = wallet.custom.account;
+      transactionRequest.prodOrig = `${config.COOPCENTRAL_VIRTUAL_ACCOUNT}`;
+
+      const bankTransaction = await this.coopcentralApi.createBankTransaction(
+        transactionRequest
+      );
+
+      console.log(JSON.stringify(bankTransaction));
+      job.status = "COMPLETED";
+    } catch (error) {
+      if (error instanceof ServiceError) {
+        if (error.isPending()) {
+          return;
+        }
+
+        if (error.isRetryable()) {
+          job.status = "PENDING";
+          return;
+        }
+      }
     }
   }
 
   async prepare(context: TransactionContext): Promise<PrepareResult> {
-    console.log(JSON.stringify(context))
-
-    const jobId = context.job.handle
-    const job = suspendedJobs.get(jobId)
+    const jobId = context.job.handle;
+    const job = suspendedJobs.get(jobId);
 
     if (!job) {
       suspendedJobs.set(jobId, {
         context,
-        status: 'PENDING',
-      })
+        status: "PENDING",
+      });
     }
 
-    if (this.isContinue(context) && job?.status === 'COMPLETED') {
-      this.cleanSuspended(context)
+    if (this.isContinue(context) && job?.status === "COMPLETED") {
+      this.cleanSuspended(context);
 
       return {
         status: JobResultStatus.Prepared,
-        coreId: '112',
+        coreId: "112",
         custom: {
-          app: 'bridge-x',
-          method: 'AsyncDebitBankAdapter.prepare',
+          app: "bridge-x",
+          method: "AsyncDebitBankAdapter.prepare",
         },
-      }
+      };
     }
 
-    if (job?.status === 'FAILED') {
-      this.cleanSuspended(context)
+    if (job?.status === "FAILED") {
+      this.cleanSuspended(context);
 
       return {
         status: JobResultStatus.Failed,
         error: job.error,
-      } as PrepareFailedResult
+      } as PrepareFailedResult;
     }
 
-    this.prepareAsync(context)
+    this.prepareAsync(context);
 
     return {
       status: JobResultStatus.Suspended,
-      suspendedUntil: new Date(Date.now() + 5000)
-    }
+      suspendedUntil: new Date(Date.now() + 5000),
+    };
   }
 
-  abort(context: TransactionContext): Promise<AbortResult> {
-    console.log('debit abort called')
-    console.log(JSON.stringify(context, null, 2))
+  async abort(context: TransactionContext): Promise<AbortResult> {
+    console.log("debit abort called");
 
-    if (this.isContinue(context)) {
-      this.cleanSuspended(context)
-
-      const result: AbortSucceededResult = {
-        status: JobResultStatus.Aborted,
-        coreId: '667',
-        custom: {
-          app: 'bridge-x',
-          method: 'AsyncDebitBankAdapter.abort',
-        },
-      }
-
-      return Promise.resolve(result)
-    } else {
-      return this.suspend(context)
-    }
-  }
-
-  async commit(context: TransactionContext): Promise<CommitResult> {
-    console.log(JSON.stringify(context))
-
-    const jobId = context.job.handle
-    const job = suspendedJobs.get(jobId)
+    const jobId = context.job.handle;
+    const job = suspendedJobs.get(jobId);
 
     if (!job) {
       suspendedJobs.set(jobId, {
         context,
-        status: 'PENDING',
-      })
+        status: "PENDING",
+      });
     }
 
-    if (this.isContinue(context) && job?.status === 'COMPLETED') {
-      this.cleanSuspended(context)
+    if (this.isContinue(context) && job?.status === "COMPLETED") {
+      this.cleanSuspended(context);
 
-      return {
-        status: JobResultStatus.Committed,
-        coreId: '112',
+      const result: AbortSucceededResult = {
+        status: JobResultStatus.Aborted,
+        coreId: "667",
         custom: {
-          app: 'bridge-x',
-          method: 'AsyncDebitBankAdapter.prepare',
+          app: "bridge-x",
+          method: "AsyncDebitBankAdapter.abort",
         },
-      } as CommitResult
+      };
     }
 
-    this.commitAsync(context)
+    this.abortAsync(context);
 
     return {
       status: JobResultStatus.Suspended,
-      suspendedUntil: new Date(Date.now() + 10000)
-    }
+      suspendedUntil: new Date(Date.now() + 5000),
+    };
+  }
+
+  async commit(context: TransactionContext): Promise<CommitResult> {
+    console.log("debit commit called");
+
+    return {
+      status: JobResultStatus.Committed,
+      coreId: "112",
+      custom: {
+        app: "bridge-x",
+        method: "AsyncDebitBankAdapter.prepare",
+      },
+    } as CommitResult;
   }
 
   protected isContinue(context: TransactionContext) {
-    return suspendedJobs.has(context.job.handle)
+    return suspendedJobs.has(context.job.handle);
   }
 
   protected cleanSuspended(context: TransactionContext) {
-    suspendedJobs.delete(context.job.handle)
-  }
-
-  protected suspend(context: TransactionContext) {
-    const job = context.job.handle
-
-    console.log(`suspending indefinitely job ${job}`)
-
-    suspendedJobs.set(job, context)
-
-    const suspendedResult: JobSuspendedResult = {
-      status: JobResultStatus.Suspended,
-    }
-
-    return Promise.resolve(suspendedResult)
+    suspendedJobs.delete(context.job.handle);
   }
 }
